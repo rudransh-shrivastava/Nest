f1c7ed1e2da24400c78732cd80eeb2f7876e6925
Rudransh Shrivastava <rudransh.shrivastava@owasp.org>
2026-06-16 15:52:39 +0530

    improve list styles

--- frontend/src/server/queries/claimQueries.ts
+++ frontend/src/server/queries/claimQueries.ts
@@ -4,6 +4,7 @@ export const GET_CANDIDATE_CLAIM = gql`
   query GetBoardCandidateClaim($login: String!, $key: String!, $year: Int!) {
     boardCandidateClaim(login: $login, key: $key, year: $year) {
       id
+      createdAt
       description
       key
       name
@@ -17,11 +18,13 @@ export const GET_CANDIDATE_CLAIMS = gql`
   query GetBoardCandidateClaims($login: String!, $year: Int!) {
     boardCandidateClaims(login: $login, year: $year) {
       id
+      createdAt
       description
       key
       name
       order
       status
+      updatedAt
     }
   }
 `
